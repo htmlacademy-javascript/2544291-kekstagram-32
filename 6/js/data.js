35f6d29336566a47b5a10f23fb5f75e565759bf9
Keks <keks@htmlacademy.ru>
2024-07-18 13:48:31 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,106 @@
+import { getRandomInteger, getRandomArrayElement, createId } from './util.js';
+
+const names = [
+  'Александр',
+  'Екатерина',
+  'Михаил',
+  'Анна',
+  'Дмитрий',
+  'София',
+  'Иван',
+  'Анастасия',
+  'Константин',
+  'Виктория',
+  'Сергей',
+  'Елена',
+  'Павел',
+  'Анатолий',
+  'Артем',
+  'Наталья',
+  'Андрей',
+  'Юлия',
+  'Максим',
+  'Мария',
+  'Владимир',
+  'Ирина',
+  'Николай',
+  'Татьяна',
+  'Станислав',
+];
+
+const descriptions = [
+  'Закат на пляже',
+  'Цветущие сакуры в парке',
+  'Горный пейзаж с облаками',
+  'Архитектурные детали старого города',
+  'Роскошный вид на городской небоскреб',
+  'Играющие дети на лужайке',
+  'Пикник на лоне природы',
+  'Разноцветные воздушные шары на фестивале',
+  'Дикие животные в своем естественном местообитании',
+  'Граффити на стене',
+  'Собрание цветов в ботаническом саду',
+  'Заснеженный лес зимой',
+  'Вдохновляющий вид с горы',
+  'Уютный интерьер кафе',
+  'Парад ярких костюмов на карнавале',
+  'Интересный уличный арт',
+  'Эмоциональный концерт в ночном клубе',
+  'Блестящие небоскребы в деловом центре',
+  'Застывшие капли дождя на стекле',
+  'Феерический закат в горах',
+  'Приглашающий вид ресторана на берегу',
+  'Динамичный городской пейзаж',
+  'Таинственный лесной уголок',
+  'Магический взгляд звездного неба в пустыне',
+  'Пленительные водопады в тропическом лесу',
+];
+
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const ValueLikes = {
+  MIN: 15,
+  MAX: 200,
+};
+
+const ValueComments = {
+  MIN: 0,
+  MAX: 30,
+};
+
+const ValueAvatars = {
+  MIN: 1,
+  MAX: 6,
+};
+
+const MAX_VALUE_PHOTOS = 25;
+
+const createIdUser = createId();
+const createIdPhoto = createId();
+const createUrlPhoto = createId();
+
+const createUsers = () => ({
+  id: createIdUser(),
+  avatar: `img/avatar-${getRandomInteger(ValueAvatars.MIN, ValueAvatars.MAX)}.svg`,
+  message: getRandomArrayElement(messages),
+  name: getRandomArrayElement(names),
+});
+
+const createPhotos = () => ({
+  id: createIdPhoto(),
+  url: `photos/${createUrlPhoto()}.jpg`,
+  description: getRandomArrayElement(descriptions),
+  likes: getRandomInteger(ValueLikes.MIN, ValueLikes.MAX),
+  comments:  Array.from({length: getRandomInteger(ValueComments.MIN, ValueComments.MAX)}, createUsers)
+});
+
+const getPhotos = () => Array.from({length: MAX_VALUE_PHOTOS}, createPhotos);
+
+export { getPhotos };
---
+++ 6/js/main.js
@@ -0,0 +1,4 @@
+import { getPhotos } from './data.js';
+import { drawPictures } from './draw-pictures.js';
+
+drawPictures(getPhotos());
